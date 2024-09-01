6f2b13316b7ebfe3c927aacf47ca5ae4f328537a
zoodaa <jkj01230@gmail.com>
2024-09-01 17:45:15 +0900

    feat: ios splash screen 구현
    - react-native-splash-screen 라이브러리 추가
    - SplashScreen Time 추가
    - SplashScreen Image 추가

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500);
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar />
