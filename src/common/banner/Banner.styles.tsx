7017cf7cc87d981554aa56785d408fc926bd21cc
zoodaa <jkj01230@gmail.com>
2024-09-07 02:18:15 +0900

    feat: Banner 구현
    - 광고 배너 컴포넌트 구현

---
+++ src/common/banner/Banner.styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 50px;
+  background: pink;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.Text`
+  color: #fff;
+  font-size: 12px;
+`;
---
+++ src/common/banner/Banner.tsx
@@ -0,0 +1,11 @@
+import * as styles from './Banner.styles.tsx';
+
+const Banner = () => {
+  return (
+    <styles.Container>
+      <styles.Text>광고 배너 자리</styles.Text>
+    </styles.Container>
+  );
+};
+
+export default Banner;
